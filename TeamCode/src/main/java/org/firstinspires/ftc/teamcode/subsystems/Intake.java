d4966a2629f1ac5d1d6b79769a2961a391268993
akashset10-hash <akashset10@gmail.com>
2026-01-02 11:36:30 -0800

    sdfsd

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -9,22 +9,24 @@ public class Intake {
     private DcMotorEx intakeMotor;
     private CRServo intakeServoRight;
     private CRServo intakeServoLeft;
+
     public Intake (HardwareMap hardwareMap) {
         intakeMotor = hardwareMap.get(DcMotorEx.class, "intakeMotor");
         intakeServoRight = hardwareMap.get(CRServo.class, "intakeServoRight");
-        intakeServoLeft = hardwareMap.get(CRServo.class, "intakeServoLeft");
+        intakeServoLeft = hardwareMap.get(CRServo.class, "intakeServoLeft"); // Line 15
 
         intakeServoRight.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
-    public void intake () {
-        intakeMotor.setPower(1);
-        intakeServoRight.setPower(1);
-        intakeServoLeft.setPower(1);
+    public void intake() {
+        intakeMotor.setPower(1.0);
+        intakeServoRight.setPower(1.0);
+        intakeServoLeft.setPower(1.0);
     }
-    public void stop () {
-        intakeMotor.setPower(0);
-        intakeServoRight.setPower(0);
-        intakeServoLeft.setPower(0);
+
+    public void stop() {
+        intakeMotor.setPower(0.0);
+        intakeServoRight.setPower(0.0);
+        intakeServoLeft.setPower(0.0);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooter.java
@@ -1,41 +1,28 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-import com.qualcomm.robotcore.hardware.CRServo; // For Continuous Rotation
-import com.qualcomm.robotcore.hardware.Servo;   // For Standard (90 degree)
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.Range;
 
 public class Shooter {
+    // Updated to 70% power as requested
     private static final double DEFAULT_SHOOTER_POWER = 0.70;
 
-    // Position 0.5 is typically 90 degrees on a standard servo
-    private static final double GATE_OPEN_90 = 0.5;
-    private static final double GATE_CLOSED = 0.0;
-
     private final DcMotorEx flywheelMotor;
-    private final Servo intakeServoLeft;
-
     private double currentMotorPower = 0.0;
 
     public Shooter(HardwareMap hardwareMap) {
         flywheelMotor = hardwareMap.get(DcMotorEx.class, "flywheelMotor");
         flywheelMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.FLOAT);
         flywheelMotor.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
-
-        // Standard Servo on Expansion Hub Port 0
-        intakeServoLeft = hardwareMap.get(Servo.class, "intakeServoLeft");
-        intakeServoLeft.setPosition(GATE_CLOSED);
     }
 
     public void shoot() {
         setPower(DEFAULT_SHOOTER_POWER);
-        intakeServoLeft.setPosition(GATE_OPEN_90); // Moves exactly to 90 degrees
     }
 
     public void stop() {
         setPower(0.0);
-        intakeServoLeft.setPosition(GATE_CLOSED); // Moves back to 0 degrees
     }
 
     public void setPower(double power) {
